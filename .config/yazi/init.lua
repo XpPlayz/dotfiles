fee469c1e532e1c7d8e14458893edb1b672a061b
XpPlayz <ronypaularun@gmail.com>
2025-07-27 18:53:02 -0500

    added yazi

---
+++ .config/yazi/init.lua
@@ -0,0 +1,46 @@
+-- ~/.config/yazi/init.lua
+function Linemode:size_and_mtime()
+	local time = math.floor(self._file.cha.mtime or 0)
+	if time == 0 then
+		time = ""
+	elseif os.date("%Y", time) == os.date("%Y") then
+		time = os.date("%b %d %H:%M", time)
+	else
+		time = os.date("%b %d  %Y", time)
+	end
+
+	local size = self._file:size()
+	return string.format("%s %s", size and ya.readable_size(size) or "-", time)
+end
+
+-- symlink status bar
+Status:children_add(function(self)
+	local h = self._current.hovered
+	if h and h.link_to then
+		return " -> " .. tostring(h.link_to)
+	else
+		return ""
+	end
+end, 3300, Status.LEFT)
+
+-- custom shell
+require("custom-shell"):setup({
+    history_path = "/home/ronaldo/.zsh_history",
+    save_history = true,
+})
+
+-- copy-file-contents.yazi
+require("copy-file-contents"):setup({
+	append_char = "\n",
+	notification = true,
+})
+
+-- symlink in status bar
+Status:children_add(function(self)
+	local h = self._current.hovered
+	if h and h.link_to then
+		return " -> " .. tostring(h.link_to)
+	else
+		return ""
+	end
+end, 3300, Status.LEFT)
