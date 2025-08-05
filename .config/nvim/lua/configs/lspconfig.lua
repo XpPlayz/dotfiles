ad7da570747bd4bddadc2e07dc06ebacef322443
XpPlayz <ronypaularun@gmail.com>
2025-08-05 15:16:51 -0500

    reformatted lspconfig.lua and added autocmds.lua

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -1,6 +1,26 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "clangd", "pyright", "ts_ls" }
-vim.lsp.enable(servers)
-
 -- read :h vim.lsp.config for changing options of lsp servers 
+local servers = {
+  html = {},
+  cssls = {},
+  ts_ls = {},
+  bashls = {},
+
+  pyright = {
+    settings = {
+      python = {
+        analysis = {
+          autoSearchPaths = true,
+          typeCheckingMode = "basic",
+        },
+      },
+    },
+  },
+}
+
+for name, opts in pairs(servers) do
+  vim.lsp.enable(name)  -- nvim v0.11.0 or above required
+  vim.lsp.config(name, opts) -- nvim v0.11.0 or above required
+end
+
